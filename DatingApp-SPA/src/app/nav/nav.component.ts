6705165c17a6790d817b7f46e8c65912eda6c018
En <a0955105@gmail.com>
2020-03-12 22:22:54 +0800

    Error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
         console.log('success');
       },
       error => {
-        console.log('fail');
+        console.log(error);
       }
     );
   }
