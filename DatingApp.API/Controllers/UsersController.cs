fd8ed55c0ae3e1011ed133d4e4af9be6cdef8f57
En <a0955105@gmail.com>
2020-03-24 20:11:45 +0800

    Many to Many

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -31,7 +32,7 @@ namespace DatingApp.API.Controllers
             var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
 
             var userFromRepo = await this.repo.GetUser(currentUserId);
-            
+
             userParams.UserId = userFromRepo.Id;
 
             if (string.IsNullOrEmpty(userParams.Gender))
@@ -68,6 +69,32 @@ namespace DatingApp.API.Controllers
                 return NoContent();
 
             throw new Exception($"Updating user {id} failed on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            if (await this.repo.GetLike(id, recipientId) != null)
+                return BadRequest("You already like this user");
+
+            if (await this.repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            var like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            this.repo.Add<Like>(like);
+
+            if (await this.repo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to like user");
         }
     }
 }
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -25,6 +25,11 @@ namespace DatingApp.API.Data
             this.context.Remove(entity);
         }
 
+        public async Task<Like> GetLike(int userId, int recipientId)
+        {
+            return await this.context.Likes.FirstOrDefaultAsync(u => u.LikerId == userId && u.LikeeId == recipientId);
+        }
+
         public async Task<Photo> GetMainPhotoForUser(int userId)
         {
             return await this.context.Photos.Where(p => p.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
@@ -52,6 +57,18 @@ namespace DatingApp.API.Data
 
             users = users.Where(u => u.Gender == userParams.Gender);
 
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u=>userLikers.Contains(u.Id));
+            }
+
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u=>userLikees.Contains(u.Id));
+            }
+
             if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
                 var minDob = DateTime.Now.AddYears(-userParams.MaxAge - 1);
@@ -74,6 +91,22 @@ namespace DatingApp.API.Data
             }
             // var users = await this.context.Users.Include(p => p.Photos).ToListAsync();
             return await PageList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await this.context.Users
+                .Include(x => x.Likers)
+                .Include(x => x.Likees)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+                return user.Likers.Select(i => i.LikerId);
+                // return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+            else
+                return user.Likees.Select(i => i.LikeeId);
+                // return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+
         }
 
         public async Task<bool> SaveAll()
